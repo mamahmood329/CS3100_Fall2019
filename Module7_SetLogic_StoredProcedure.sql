5455a721096521780ad340e4e5473da1680ef15e
Xuan Pham <xhp4y8@gmail.com>
2019-10-26 12:28:14 -0500

    Create Module7_SetLogic_StoredProcedure.sql

---
+++ Module7_SetLogic_StoredProcedure.sql
@@ -0,0 +1,47 @@
+-- SET LOGIC
+
+
+SELECT
+o.orderDate AS 'Date',
+'Order' AS 'Type'
+FROM orders o
+WHERE o.customerNumber = 114
+
+UNION
+
+SELECT
+paymentDate AS 'Date',
+'Payment' AS 'Type'
+FROM payments
+WHERE customerNumber = 114;
+
+-- What is the difference between UNION and UNION ALL?
+
+-- STORED PROCEDURE
+
+-- CREATING A STORED PROCEDURE
+
+DELIMITER $$
+CREATE PROCEDURE CustomerPhoneProcedure (custNum INT)
+BEGIN
+SELECT
+customerNumber,
+customerName,
+phone
+FROM customers
+WHERE customerNumber = custNum;
+END $$
+DELIMITER;
+
+-- USING A STORED PROCEDURE
+
+CALL CustomerPhoneProcedure(103);
+
+-- REMOVING STORED PROCEDURE
+
+DROP PROCEDURE CustomerPhoneProcedure;
+
+-- QUICK EXERCISE 
+
+
+
